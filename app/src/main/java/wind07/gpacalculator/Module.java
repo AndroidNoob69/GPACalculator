6b3d81e38b3f96cfcf65918af74137cf48b85a92
Carlsen Teo <carlsen@inspirous.us>
2019-06-27 16:32:15 +0800

    Module: Narrow down access modifier
    Signed-off-by: Carlsen Teo <carlsen@inspirous.us>

--- app/src/main/java/wind07/gpacalculator/Module.java
+++ app/src/main/java/wind07/gpacalculator/Module.java
@@ -4,11 +4,11 @@ package wind07.gpacalculator;
  * Created by carlsen on 5/7/2017.
  */
 
-public class Module {
+class Module {
     String modName;
     double credUnits;
     String grade;
-    public Module(String ModName, double CredUnits, String Grade){
+    Module(String ModName, double CredUnits, String Grade){
         modName = ModName;
         credUnits = CredUnits;
         grade = Grade;
